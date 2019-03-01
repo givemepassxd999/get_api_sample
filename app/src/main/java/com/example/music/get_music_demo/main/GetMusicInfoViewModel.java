6c83e8454ff06b6ebb28516dad99b5f9eeb02e73
Rick <givemepassxd999@gmail.com>
2019-03-02 06:46:30 +0800

    [Feature] 加入 MVVM

--- app/src/main/java/com/example/music/get_music_demo/main/GetMusicInfoViewModel.java
+++ app/src/main/java/com/example/music/get_music_demo/main/GetMusicInfoViewModel.java
@@ -1,18 +1,74 @@
 package com.example.music.get_music_demo.main;
 
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
+import android.content.Context;
+import android.support.annotation.Nullable;
 
 import com.example.music.get_music_demo.connection.api.MusicInfoResponse;
+import com.example.music.get_music_demo.database.MusicInfo;
+import com.example.music.get_music_demo.database.MusicInfoDBHelper;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GetMusicInfoViewModel extends ViewModel {
     private GetMusicInfoDataModel dataModel;
-
+    private ExecutorService executorService;
+    public MutableLiveData<List<MusicInfo>> musicDatas = new MutableLiveData<>();
+    private MusicInfoDBHelper musicInfoDBHelper;
+    private Context context;
     public GetMusicInfoViewModel(GetMusicInfoDataModel dataModel) {
         this.dataModel = dataModel;
+        musicInfoDBHelper = new MusicInfoDBHelper();
+        executorService = Executors.newSingleThreadExecutor();
     }
 
-    public MutableLiveData<MusicInfoResponse> getMusicInfo(String value){
-        return dataModel.getMusicInfo(value);
+    public void setContext(Context context){
+        this.context = context;
+        musicInfoDBHelper.createDB(context);
+    }
+
+    public void callGetMusicInfoApi(final String query){
+        dataModel.getMusicInfo(query).observe((MainActivity)context, new Observer<MusicInfoResponse>() {
+            @Override
+            public void onChanged(@Nullable MusicInfoResponse musicInfoResponse) {
+                if(musicInfoResponse.isHttpSuccess()){
+                    if(musicInfoResponse.isSuccess()){
+                        final List<MusicInfo> musicInfos = musicInfoResponse.getMusicInfos();
+                        if(musicInfos != null) {
+                            musicDatas.setValue(musicInfos);
+                            executorService.submit(new Runnable() {
+                                @Override
+                                public void run() {
+                                    for (MusicInfo musicInfo : musicInfos) {
+                                        musicInfo.setKeyword(query);
+                                        musicInfoDBHelper.insertData(musicInfo);
+                                    }
+                                }
+                            });
+                        }
+                    } else{
+                        queryDb(query);
+                    }
+                } else{
+                    queryDb(query);
+                }
+            }
+        });
+    }
+
+    private void queryDb(final String query){
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                final List<MusicInfo> musicInfos = musicInfoDBHelper.queryData(query);
+                if(musicInfos != null) {
+                    musicDatas.setValue(musicInfos);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/music/get_music_demo/main/MainActivity.java
+++ app/src/main/java/com/example/music/get_music_demo/main/MainActivity.java
@@ -1,41 +1,37 @@
 package com.example.music.get_music_demo.main;
 
-import android.app.SearchManager;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.example.music.get_music_demo.R;
-import com.example.music.get_music_demo.connection.api.MusicInfoResponse;
 import com.example.music.get_music_demo.database.MusicInfo;
-import com.example.music.get_music_demo.database.MusicInfoDBHelper;
+import com.example.music.get_music_demo.databinding.ActivityMainBinding;
+import com.roger.catloadinglibrary.CatLoadingView;
 
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import es.dmoral.toasty.Toasty;
 
 public class MainActivity extends AppCompatActivity {
-
     private GetMusicInfoViewModel getBalanceViewModel;
     private RecyclerView recyclerView;
     private MusicInfoAdapter adapter;
-    private MusicInfoDBHelper musicInfoDBHelper;
-    private ExecutorService executorService;
+    private ActivityMainBinding binding;
+    private CatLoadingView loadingView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initData();
         initView();
     }
@@ -43,66 +39,35 @@ public class MainActivity extends AppCompatActivity {
     private void initData() {
         GetMusicInfoFactory getMusicInfoFactory = new GetMusicInfoFactory();
         getBalanceViewModel = ViewModelProviders.of(this, getMusicInfoFactory).get(GetMusicInfoViewModel.class);
-        musicInfoDBHelper = new MusicInfoDBHelper();
-        musicInfoDBHelper.createDB(this);
-        executorService = Executors.newSingleThreadExecutor();
+        getBalanceViewModel.setContext(this);
     }
 
     private void initView() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        loadingView = new CatLoadingView();
+        binding.toolbar.setTitle("");
+        binding.toolbar.setNavigationIcon(R.drawable.ic_adb_black_24dp);
+        binding.searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                // Handle the menu item
-                return true;
-            }
-        });
-        toolbar.inflateMenu(R.menu.main);
-        toolbar.setTitle("");
-        toolbar.setNavigationIcon(R.drawable.ic_adb_black_24dp);
-        setSupportActionBar(toolbar);
-        recyclerView = findViewById(R.id.recycler_view);
-        adapter = new MusicInfoAdapter(this);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(adapter);
-    }
-
-    private void callGetMusicInfoApi(final String query){
-        getBalanceViewModel.getMusicInfo(query).observe(this, new Observer<MusicInfoResponse>() {
-            @Override
-            public void onChanged(@Nullable MusicInfoResponse musicInfoResponse) {
-                if(musicInfoResponse.isHttpSuccess()){
-                    if(musicInfoResponse.isSuccess()){
-                        final List<MusicInfo> musicInfos = musicInfoResponse.getMusicInfos();
-                        if(musicInfos != null) {
-                            adapter.setData(musicInfos);
-                            adapter.notifyDataSetChanged();
-                            executorService.submit(new Runnable() {
-                                @Override
-                                public void run() {
-                                    for(MusicInfo musicInfo : musicInfos){
-                                        musicInfo.setKeyword(query);
-                                        musicInfoDBHelper.insertData(musicInfo);
-                                    }
-                                }
-                            });
-                        }
-                    } else{
-                        queryDb(query);
-                    }
-                } else{
-                    queryDb(query);
+            public void onClick(View v) {
+                String s = binding.searchEdit.getText().toString();
+                if(TextUtils.isEmpty(s)) {
+                    Toasty.warning(getApplicationContext(), R.string.input_search_text).show();
+                } else {
+                    getBalanceViewModel.callGetMusicInfoApi(s);
+                }
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
                 }
             }
         });
-    }
-
-    private void queryDb(final String query){
-        executorService.submit(new Runnable() {
+        adapter = new MusicInfoAdapter(this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        binding.recyclerView.setLayoutManager(linearLayoutManager);
+        binding.recyclerView.setAdapter(adapter);
+        getBalanceViewModel.musicDatas.observe(this, new Observer<List<MusicInfo>>() {
             @Override
-            public void run() {
-                final List<MusicInfo> musicInfos = musicInfoDBHelper.queryData(query);
+            public void onChanged(@Nullable List<MusicInfo> musicInfos) {
                 if(musicInfos != null) {
                     adapter.setData(musicInfos);
                     adapter.notifyDataSetChanged();
@@ -110,35 +75,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        MenuItem menuSearchItem = menu.findItem(R.id.search);
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        final SearchView searchView = (SearchView) menuSearchItem.getActionView();
-        searchView.setSubmitButtonEnabled(true);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(true);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                callGetMusicInfoApi(s);
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                if (imm != null) {
-                    imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
-                }
-                searchView.clearFocus();
-                return true;
-
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
-            }
-        });
-        return true;
-    }
-
 }
